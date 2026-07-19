49db039fad655551bb006493d1ce2e7628e2dc7d
Aryan Singh <coxaryan.singh@gmail.com>
2026-07-19 14:53:54 +0530

    Today's We started Javascript let's Goooo! 🥳🎉🎊

---
+++ Video 47/script.js
@@ -0,0 +1,11 @@
+// alert("Hello Guys, today We're Stared the Javascript Tutorial with Harry bhaiya!❤️")
+
+// prompt("hello guy's really you want to go further");
+// prompt("enter number")
+// console.log("Hello World!")
+
+
+let a = 4;
+let b = 5;
+let zs = 1;
+console.log(a+b+zs);
