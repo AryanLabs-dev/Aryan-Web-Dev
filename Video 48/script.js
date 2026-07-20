edabbb84be47e896392ddc3e4d24c1fee28ea7d8
Aryan Singh <coxaryan.singh@gmail.com>
2026-07-20 21:28:57 +0530

    Quick Quiz; n javascript ✅

---
+++ Video 48/script.js
@@ -0,0 +1,13 @@
+// Quick Quiz; write a javascript program to store name phone number and marks of a student using objects. let's goooo!
+
+
+let student = {
+    Name: "Akhand Pratap Singh",
+    "Phone Number": "7054783582",
+    Marks: "80%"
+};
+
+let { Name, Marks } = student;
+let phone = student["Phone Number"];
+
+console.log(Name + " " + phone + " " + Marks);node 
