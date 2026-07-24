7301b3d1e12af3004b0d2aa8db29b865d04050f7
Aryan Singh <coxaryan.singh@gmail.com>
2026-07-24 23:33:17 +0530

    Commit all Javascript tutorial files!

---
+++ Video 49/index.js
@@ -0,0 +1,23 @@
+ let a = 10;
+ let b = 30;
+ let c = 3;
+ console.log(a+b+c)
+
+
+let age = 15;
+let agediscount = 3;
+
+if (age == 18) {
+    console.log("You are able to Vote 🙁")
+}
+else if (age + agediscount == 18) {
+    console.log("You are able to Vote 🎊")
+}
+
+// do {
+    
+// } while (condition);
+
+// while () {
+    
+// }
---
+++ Video 50/index.js
@@ -0,0 +1,24 @@
+// let aryan = 1;
+
+// for (let Aryan = 0; Aryan <= 100000;  Aryan++) {
+//     console.log(aryan+Aryan)
+// }
+
+// for (let Aryan = 1; Aryan < 1000; Aryan++) {
+//     console.log((Aryan + ". Aryan Bhaiya!"));
+// }
+
+// let Aryan = 1;
+
+// for (const key in Aryan) {
+//     if (!Object.hasOwn(Aryan, key)) continue;
+    
+//     const element = Aryan[key];
+//     console.log(Aryan,key)
+// }
+let Aryan = 1;
+for (const element of Aryan) {
+    name: "Aryan Singh"
+    company: "Aryan Industries"
+    console.log( Aryan,element)
+}
