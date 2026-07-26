cfd904a71978d9148d1d4616e8745e1b642d4dba
Aryan Singh <coxaryan.singh@gmail.com>
2026-07-26 14:33:00 +0530

    JavaScript Functions Completed by AryanLabs ✅🎊

---
+++ Video 51/Index.js
@@ -0,0 +1,42 @@
+function Aryan(Myusers) {
+    console.log("Hello " + Myusers + " What Are u Doing bro 🎉")
+    console.log("Hello " + Myusers + " What Are u Doing bro 🎉")
+    console.log("Hello " + Myusers + " What Are u Doing bro 🎉")
+    console.log("Hello " + Myusers + " What Are u Doing bro 🎉")
+}
+
+Aryan("Aryan")
+
+Aryan("Mr. Akhand Pratap Singh")
+
+Aryan("Mark")
+
+Aryan("Mr. Beast")
+
+function sum(a, b, c = 5) {
+    console.log(a + b + c)
+}
+sum(5, 10)
+sum(15,5)
+function Myusers(Name) {
+    return "hello👋 " + Name + " Congratulations & Blessings for Your Coding Journey 🎉😊"
+}
+console.log (Myusers("Aryan"));
+console.log (Myusers("Akhand Pratap Singh"));
+console.log (Myusers("Mr beast"));
+console.log (Myusers("Mark"));
+console.log (Myusers("Admin"));
+
+
+function Sum(a,b,c) {
+    console.log(a * b + c)
+}
+Sum(5,6,8)
+Sum(5,7,-2)
+
+
+function Sum(a,b) {
+    console.log(a*b)
+}
+let Aryan1 = Sum(140,3);
+console.log(Aryan1)
