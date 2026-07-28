e5f79a3f207fe0954f80ca72d4d99e78f011bd13
Aryan Singh <coxaryan.singh@gmail.com>
2026-07-28 20:09:24 +0530

    JavaScript Strings Successfully✅

---
+++ Video 53/index.js
@@ -0,0 +1,38 @@
+let a = "Aryan"
+let s = "Singh"
+let i = "😊"
+console.log(a + " " + s + " " + i);
+
+let me1 = "Aryan";
+
+console.log(`Hello ${me1}`);
+
+let name = "aryan";
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());;
+let meo = " Aryan ";
+console.log(meo.trim());
+
+let admin = "Aryan Singh";
+console.log(admin.replace("Singh", "Sir 😊"));
+
+let newo = "I love JavaScript 12";
+console.log(newo.includes("JavaScript"))
+console.log(newo.includes("python"))
+
+let myis = "Aryan";
+
+console.log(myis.slice(0, 4))
+
+
+let me = "Aryan Sir"
+
+console.log(me.length)
+console.log(me.toUpperCase())
+console.log(me.replace("Aryan", "Akhand"))
+console.log(me.includes("Akhand"))
+console.log(me.includes("Aryan"))
+console.log(me.slice(0, 9))
+
+let I = "Aryan"
+console.log(`hello ${I}`)
